1de0e0218f70ef49f722b2d33cc791d17e214ad8
Nathan Michalov <nmichalov@gmail.com>
2012-04-17 17:56:28 -0400

    Now reading data from a server, but not yet in real time

--- javascripts/graph-data.js
+++ javascripts/graph-data.js
@@ -1,14 +1,25 @@
-var rawData = "http://0.0.0.0:8888/data.json"; 
+//Data URL
+var rawData  = "http://0.0.0.0:8888/data.json";
 
-var width = 910,
+
+//Set Global Variables
+var width  = 910,
     height = 300,
-    links = [];
+    graphNodes  =  [],
+    graphLinks  =  [];
 
-var force = d3.layout.force()
-    .size([width, height])
-    .linkDistance(100)
-    .charge(-120);
 
+//Create Instance of Above Object for Graph's Central Node
+//And Push To graphNodes Array
+graphNodes[0] = {"name": "Lantern", "activity" : 70 };
+
+
+//Function For Processing The Incoming Data
+//function 
+
+
+
+//Initialize Visualization    
 var vis = d3.select(".data").append("svg:svg")
     .attr("width", width)
     .attr("height", height);
@@ -17,16 +28,50 @@ vis.append("svg:rect")
     .attr("width", width)
     .attr("height", height);
 
-$.getJSON("http://0.0.0.0:8888/data.json", function(jd) { 
-    var data = jd.data;
-    console.log(data);
+var force = d3.layout.force()
+    .size([width, height])
+    .linkDistance(100)
+    .charge(-120);
 
-    data.forEach(function(target) {
-        links.push({source: data[0], target: target});
+//Get Initial Data And Create Graph
+//The Assumption Is That This Data Comes As An Array Of JSON Objects
+$.getJSON(rawData, function(data) {
+    $.each(data, function(key, val){
+        graphNodes.push(val);
     });
- 
-    var link = vis.selectAll("line.link")
-        .data(links)
+
+    for (i=1;i<graphNodes.length;i++){
+        var arc  =  {"source": graphNodes[0], "target": graphNodes[i]}; 
+        if (!(graphLinks.hasOwnProperty(arc))){
+            graphLinks.push(arc);
+        }
+    }
+
+    force
+        .nodes(graphNodes)
+        .links(graphLinks)
+        .start();
+
+    var node = vis.selectAll("g.node")
+        .data(graphNodes)
+      .enter().append("svg:g")
+        .attr("class", "node")
+        .call(force.drag);
+
+    node.append("svg:image")
+            .attr("class", "circle")
+            .attr("xlink:href", function(d) { return "imgs/"+d.name; })
+            .attr("x"  ,        function(d) { return -d.activity/2; })
+            .attr("y"  ,        function(d) { return -d.activity/2; })
+            .attr("height",     function(d) { return d.activity;  })
+            .attr("width",      function(d) { return d.activity;  })
+            .attr("dx", 9)
+            .attr("dy", ".25em");
+    node.append("title")
+            .text(function(d) { return d.name+'\n'+d.activity+' users'; });
+
+   var link = vis.selectAll("line.link")
+        .data(graphLinks)
       .enter().append("svg:line")
         .attr("class", "link")
         .attr("x1", function(d) { return d.source.x; })
@@ -34,41 +79,11 @@ $.getJSON("http://0.0.0.0:8888/data.json", function(jd) {
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
 
-    var node = vis.selectAll("g.node")
-        .data(data)
-      .enter().append("svg:g")
-        .attr("class", "node")
-        .call(force.drag);
-      
-    force
-        .nodes(data)
-        .links(links)
-        .on("tick", tick)
-        .start();
-
-    function tick() {
-
-        node.append("svg:image")
-            .attr("class", "circle")
-            .attr("xlink:href", function(d) { return "imgs/"+d.country; })
-            .attr("x", function(d) { return -d.count/2; })
-            .attr("y", function(d) { return -d.count/2; })
-            .attr("height", function(d) { return d.count;  })
-            .attr("width",  function(d) { return d.count;  })
-            .attr("dx", 9)
-            .attr("dy", ".25em");
-
-        node.append("title")
-            .text(function(d) { return d.country+'\n'+d.count+' users'; });
-
-       force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-
-        node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-        });
-    };
+   force.on("tick", function() {
+       link.attr("x1", function(d) { return d.source.x; })
+           .attr("y1", function(d) { return d.source.y; })
+           .attr("x2", function(d) { return d.target.x; })
+           .attr("y2", function(d) { return d.target.y; });
+       node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+   });
 });
-
